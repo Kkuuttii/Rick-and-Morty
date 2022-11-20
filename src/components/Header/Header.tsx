799bb577e5edb7b89404fb5e4f8a7813108eaeac
Kkuuttii <katerinaugolek@gmail.com>
2022-11-20 23:46:34 +0300

    Создала структуру сайта. Сверстала Main Page. Создала компонент кнопки.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,15 @@
 import './Header.css';
 import logo from "../../images/logo.png"
+import Button from '../Button/Button';
 
 function Header() {
     return (
         <div className='header'>
             <div>
-                <img src={logo} alt="Main Page" />
+                <img src={logo} alt="Main Page" className='logo'/>
             </div>
             <div>
-                <button>About</button>
+                <Button className='header-button'/>
             </div>
         </div>
     )
--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,25 +1,24 @@
 import './MainPage.css';
 import mainImage from "../../images/mainImage.png"
+import Button from '../Button/Button';
 
 function MainPage() {
     return (
         <div className='main-page'>
-            <div>
-                 <div>
-                    <h1>Rick and Morty</h1>
+            <div className='info-box'>
+                 <div className='text-box'>
+                    <h1>Rick and Morty club</h1>
                     <p>The show revolves around the adventures of the members of the Smith household, which consists of parents Jerry and Beth, their children Summer and Morty, and Beth's father, Rick Sanchez, who lives with them as a guest. According to Justin Roiland, the family lives outside of Seattle, Washington.The adventures of Rick and Morty, however, take place across an infinite number of realities, with the characters travelling to other planets and dimensions through portals and Rick's flying saucer.</p>
                 </div>
-                <div>
-                    <button>Library</button>
-                    <button>Quiz</button>
+                <div className='button-wrapper'>
+                    <Button/>
+                    <Button/>
                 </div>
-                <div>
-                    <img src={mainImage} alt="RickAndMorty" />
-                </div>
+
             </div>
 
-            <div>
-                    
+            <div className='main-image-container'>
+                <img src={mainImage} alt="RickAndMorty" />
             </div>
         </div>
     )
