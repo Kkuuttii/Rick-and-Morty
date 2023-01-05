78265f50f9815bd98679f93ff0070bcae15c057d
Kkuuttii <katerinaugolek@gmail.com>
2023-01-05 18:39:51 +0300

    Удалила комментарии и console.log

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -9,28 +9,19 @@ interface ITooltip {
     maxLength: number;
 }
 
-
-
 function Tooltip({children, content, className, maxLength}: ITooltip) {
     const [isShowingContent, setIsShowingContent] = useState<boolean>(false);
-
 
     return (
         <div className="tooltip">
             <div 
-                onMouseEnter={(event)=>{
-                    setIsShowingContent(true); 
-                    console.log(isShowingContent, content.length); 
-                    console.log(event)}
-                } 
-                onMouseLeave={()=>setIsShowingContent(false)}
-            >
+                onMouseEnter={(event)=>setIsShowingContent(true)} 
+                onMouseLeave={()=>setIsShowingContent(false)}>
                 {children}
             </div>
 
             {(isShowingContent && content.length>maxLength) && <div className={classNames("pop-up-window", className)}>{content}</div>} 
         </div>
-        
     )
 }
 export default Tooltip;
