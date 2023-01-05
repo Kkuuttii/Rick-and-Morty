170a3ec2416a0fd302843d01da8b74ac59a238ef
Kkuuttii <katerinaugolek@gmail.com>
2023-01-05 18:00:19 +0300

    Создала компонент всплывающей подсказки Tooltip

---
+++ src/components/Tooltip/Tooltip.tsx
@@ -0,0 +1,36 @@
+import './Tooltip.css';
+import { useState } from 'react';
+import classNames from 'classnames';
+
+interface ITooltip {
+    children: JSX.Element;
+    content: string;
+    className?: string;
+    maxLength: number;
+}
+
+
+
+function Tooltip({children, content, className, maxLength}: ITooltip) {
+    const [isShowingContent, setIsShowingContent] = useState<boolean>(false);
+
+
+    return (
+        <div className="tooltip">
+            <div 
+                onMouseEnter={(event)=>{
+                    setIsShowingContent(true); 
+                    console.log(isShowingContent, content.length); 
+                    console.log(event)}
+                } 
+                onMouseLeave={()=>setIsShowingContent(false)}
+            >
+                {children}
+            </div>
+
+            {(isShowingContent && content.length>maxLength) && <div className={classNames("pop-up-window", className)}>{content}</div>} 
+        </div>
+        
+    )
+}
+export default Tooltip;
