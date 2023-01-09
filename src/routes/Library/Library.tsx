75706203dc3490ba1a4f818498e3ae45820d9dc6
Kkuuttii <katerinaugolek@gmail.com>
2023-01-09 15:06:30 +0300

    Сверстала поле фильтрации

--- src/routes/Library/Library.tsx
+++ src/routes/Library/Library.tsx
@@ -5,6 +5,7 @@ import {getEpisodeNumber} from '../../utils/Global'
 import axios from 'axios';
 import EpisodeCard from "../../components/EpisodeCard/EpisodeCard"
 import Pagination from '../../components/Pagination/Pagination'
+import SearchField from '../../components/SearchField/SearchField';
 
 interface ICharacterInfo {
   "id": number,
@@ -35,7 +36,11 @@ function Library() {
   const [showEpisodeCard, setShowEpisodeCard] = useState<boolean>(false);
   const [episodeInfo, setEpisodeInfo] = useState<string>("");
   const [pages, setPages] = useState<number>(1);
-  const [currentPage, setCurrentPage] = useState<number>(1);        
+  const [currentPage, setCurrentPage] = useState<number>(1);   
+  
+  //мои изменения
+  const [currentFilterName, setCurrentFilterName] = useState<string>("");
+  const [startFiltering, setStartFiltering] = useState<boolean>(false);
 
   const handlerClickCharacter = (episodeURL: any) => {
     setShowEpisodeCard(true);
@@ -47,14 +52,29 @@ function Library() {
   }
 
   async function getCharacterInfo () {
-      let response = await axios.get(`https://rickandmortyapi.com/api/character/?page=${currentPage}`);
+      let response = await axios.get(`https://rickandmortyapi.com/api/character/?page=${currentPage}&name=${currentFilterName}`);
+
       setCharacterInfo(response.data.results);
       setPages(response.data.info.pages)
   }
 
+  // //тут мои изменения!
+  // async function getFilterdCharacterInfo (name: string) {
+  //   let response = await axios.get(`https://rickandmortyapi.com/api/character/?name=${currentFilterName}`);
+  //   // currentFilterName(name);
+  //   setCharacterInfo(response.data.results);
+  //   setPages(response.data.info.pages)
+  // }
+
   useEffect (()=> {
     getCharacterInfo();
   }, [currentPage]);
+
+// // что происходит?
+//   useEffect (()=> {
+//     getFilterdCharacterInfo();
+//   }, [currstartFilteringentPage]);
+
  
   let characterCards = useMemo(()=>{
     return characterInfo.map((item: ICharacterInfo) => {
@@ -72,6 +92,9 @@ function Library() {
         <div className="library-title">
           <h2 >Character library</h2>
         </div> 
+
+        {/* мои изменения */}
+        <SearchField onHandleSearch={(name) => setCurrentFilterName (name)}/>
 
         <div className="character-cards">
           {characterCards}
