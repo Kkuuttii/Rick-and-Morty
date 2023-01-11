9b99ed3c7e9e714b5ef79d6bf069aee584f80f54
Kkuuttii <katerinaugolek@gmail.com>
2023-01-11 17:45:27 +0300

    Продолжила верстку Quiz

--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -1,12 +1,13 @@
 // import mainImage from "../../images/mainImage.png"
 import Button from '../../components/Button/Button';
+import './CheckBox.css';
 
 function CheckBox() {
     return (
-        <div>
-            <Button text='answer 1' onClick={()=>console.log('answer1')}/>
-            <Button text='answer 2' onClick={()=>console.log('answer2')}/>
-            <Button text='answer 3' onClick={()=>console.log('answer3')}/>
+        <div className='check-box'>
+            <Button text='answer 1' onClick={()=>console.log('answer1')} className="checkbox-button"/>
+            <Button text='answer 2' onClick={()=>console.log('answer2')} className="checkbox-button"/>
+            <Button text='answer 3' onClick={()=>console.log('answer3')} className="checkbox-button"/>
         </div>
     )
 }
