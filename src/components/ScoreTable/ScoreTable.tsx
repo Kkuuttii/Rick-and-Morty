23c40bccae9d6f8caa98e166f879ae90a406cdc6
Kkuuttii <katerinaugolek@gmail.com>
2023-05-23 14:20:08 +0300

    Add to gh-pages

--- src/components/ScoreTable/ScoreTable.tsx
+++ src/components/ScoreTable/ScoreTable.tsx
@@ -1,7 +1,7 @@
-import './ScoreTable.css';
-import { useState, useEffect, useMemo} from 'react';
-import {randomIntegerNumber} from '../../utils/Global';
-import Button from '../Button/Button';
+import "./ScoreTable.css";
+import { useState, useEffect, useMemo } from "react";
+import { randomIntegerNumber } from "../../utils/Global";
+import Button from "../Button/Button";
 import mindCloud from "../../images/quizImages/mind-cloud.png";
 
 import thoughtfulMorty from "../../images/quizImages/thoughtful-morty.png";
@@ -12,137 +12,140 @@ import thoughtfulRick from "../../images/quizImages/thoughtful-rick.png";
 import happyRick from "../../images/quizImages/happy-rick.png";
 import angryRick from "../../images/quizImages/angry-rick.png";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowsRotate } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowsRotate } from "@fortawesome/free-solid-svg-icons";
 
-interface IScoreTable{
-    wins: number;
-    losses: number;
-    round: number;
+interface IScoreTable {
+  wins: number;
+  losses: number;
+  round: number;
 }
 
-interface ImortyEmotions{
-    thoughtful: string,
-    happy: string,
-    angry: string,
+interface ImortyEmotions {
+  thoughtful: string;
+  happy: string;
+  angry: string;
 }
 
-interface IRickEmotions extends ImortyEmotions {
-}
+interface IRickEmotions extends ImortyEmotions {}
 
-const arrowsRotate = <FontAwesomeIcon icon={faArrowsRotate} />
-    
-function ScoreTable({wins, losses, round}: IScoreTable) {
-    const [isReactedCharacterChange, setIsReactedCharacterChange] = useState<boolean>(true);
-    const [numberOfWins, setNumberOfWins] = useState<number>(3);
-    const [numberOfLosses, setNumberOfLosses] = useState<number>(2);
-    const [score, setScore] = useState<string>("equalScore");
-    const [roundCount, setRoundCount] = useState<number>(1);
+const arrowsRotate = <FontAwesomeIcon icon={faArrowsRotate} />;
 
-    useEffect (()=> {
-        setNumberOfWins(wins);
-        setNumberOfLosses(losses);
-        setRoundCount(round);
-      }, [wins, losses, round]);
+function ScoreTable({ wins, losses, round }: IScoreTable) {
+  const [isReactedCharacterChange, setIsReactedCharacterChange] =
+    useState<boolean>(true);
+  const [numberOfWins, setNumberOfWins] = useState<number>(3);
+  const [numberOfLosses, setNumberOfLosses] = useState<number>(2);
+  const [score, setScore] = useState<string>("equalScore");
+  const [roundCount, setRoundCount] = useState<number>(1);
 
-    useEffect (()=> {
-        if(numberOfWins===numberOfLosses){
-            setScore("equalScore");
-        } else if (numberOfWins>numberOfLosses){
-            setScore("win");
-        } else {
-            setScore("loose");
-        }
-    }, [numberOfWins, numberOfLosses]);
+  useEffect(() => {
+    setNumberOfWins(wins);
+    setNumberOfLosses(losses);
+    setRoundCount(round);
+  }, [wins, losses, round]);
 
-    const comment: string = useMemo(()=>{
-        const startCommentsGeneration = () => {
-            const startComment1 = 'Are you sure you win?..';
-            const startComment2 = 'This will be difficult...';
-            const startComment3 = 'Lucky, lucky, lucky, lucky...';
-            return [startComment1, startComment2, startComment3]
-        }
-        
-        const winningCommentsGeneration = () => {
-            const winningComment1 = 'You are close to winning!';
-            const winningComment2 = 'Winning is in your pocket!';
-            const winningComment3 = 'You will definitely win!';
-            return [winningComment1, winningComment2, winningComment3];
-        }
+  useEffect(() => {
+    if (numberOfWins === numberOfLosses) {
+      setScore("equalScore");
+    } else if (numberOfWins > numberOfLosses) {
+      setScore("win");
+    } else {
+      setScore("loose");
+    }
+  }, [numberOfWins, numberOfLosses]);
 
-        const losingCommentsGeneration = () => {
-            const losingComment1 = "You really didn't know that?";
-            const losingComment2 = 'Even I know that!';
-            const losingComment3 = 'Better luck next time...';
-            return [losingComment1, losingComment2, losingComment3];
-        }
+  const comment: string = useMemo(() => {
+    const startCommentsGeneration = () => {
+      const startComment1 = "Are you sure you win?..";
+      const startComment2 = "This will be difficult...";
+      const startComment3 = "Lucky, lucky, lucky, lucky...";
+      return [startComment1, startComment2, startComment3];
+    };
 
-        const startComments = startCommentsGeneration();
-        const winningComments = winningCommentsGeneration() ;
-        const losingComments = losingCommentsGeneration();
-    
-        let comment = '';
-    
-        if(numberOfWins === numberOfLosses) {
-            comment =  startComments[randomIntegerNumber(0, startComments.length)];
-        } else if (numberOfWins > numberOfLosses) {
-            comment = winningComments[randomIntegerNumber(0, startComments.length)];
-        } else {
-            comment = losingComments[randomIntegerNumber(0, startComments.length)];
-        }
+    const winningCommentsGeneration = () => {
+      const winningComment1 = "You are close to winning!";
+      const winningComment2 = "Winning is in your pocket!";
+      const winningComment3 = "You will definitely win!";
+      return [winningComment1, winningComment2, winningComment3];
+    };
 
-        return comment;
-    }, [numberOfWins, numberOfLosses, isReactedCharacterChange])
+    const losingCommentsGeneration = () => {
+      const losingComment1 = "You really didn't know that?";
+      const losingComment2 = "Even I know that!";
+      const losingComment3 = "Better luck next time...";
+      return [losingComment1, losingComment2, losingComment3];
+    };
 
-    const emotionsEmage: string = useMemo(()=>{
-        if (isReactedCharacterChange===true) {
-            if(score==="equalScore") {
-                return thoughtfulMorty;
-            } else if(score==="win") {
-                return happyMorty;
-            } else {
-                return angryMorty;
-            }
-        } else {
-            if(score==="equalScore") {
-                return thoughtfulRick
-            } else if(score==="win") {
-                return happyRick
-            } else {
-                return angryRick
-            }
-        }
-    }, [score, isReactedCharacterChange])
+    const startComments = startCommentsGeneration();
+    const winningComments = winningCommentsGeneration();
+    const losingComments = losingCommentsGeneration();
 
-    return (
-        <div className="score-table">
-            <div className="round-counter">
-                <h2 className="rick-and-morty-font"> {((roundCount<4) && roundCount) || 3}
-                    round
-                </h2>
-                <h3 className="rick-and-morty-font">
-                    rounds remaining: {((roundCount<4) && 4-roundCount)|| 0}
-                </h3>
-            </div>
+    let comment = "";
 
-            <div className="comment-block">
-                <img src={mindCloud} alt="mind-cloud" className="mind-cloud"/>
-                <div className="comment rick-and-morty-font">
-                    {comment}
-                </div>
-            </div>
-            
-            <div className="emotions-blosk">
-                <img src={emotionsEmage} alt="emotions-pic" className="emotion-pic"/>
-                <Button text={arrowsRotate} onClick={()=>setIsReactedCharacterChange(prev => !prev)} className="change-character"/>
-            </div>
+    if (numberOfWins === numberOfLosses) {
+      comment = startComments[randomIntegerNumber(0, startComments.length)];
+    } else if (numberOfWins > numberOfLosses) {
+      comment = winningComments[randomIntegerNumber(0, startComments.length)];
+    } else {
+      comment = losingComments[randomIntegerNumber(0, startComments.length)];
+    }
 
-            <div className="score rick-and-morty-font">
-                <p>wins: {numberOfWins}</p>
-                <p>losses: {numberOfLosses}</p>
-            </div>
-            
-        </div>
-    )
+    return comment;
+  }, [numberOfWins, numberOfLosses, isReactedCharacterChange]);
+
+  const emotionsEmage: string = useMemo(() => {
+    if (isReactedCharacterChange === true) {
+      if (score === "equalScore") {
+        return thoughtfulMorty;
+      } else if (score === "win") {
+        return happyMorty;
+      } else {
+        return angryMorty;
+      }
+    } else {
+      if (score === "equalScore") {
+        return thoughtfulRick;
+      } else if (score === "win") {
+        return happyRick;
+      } else {
+        return angryRick;
+      }
+    }
+  }, [score, isReactedCharacterChange]);
+
+  return (
+    <div className="score-table">
+      <div className="round-counter">
+        <h2 className="rick-and-morty-font">
+          {" "}
+          {(roundCount < 4 && roundCount) || 3}
+          round
+        </h2>
+        <h3 className="rick-and-morty-font">
+          rounds remaining: {(roundCount < 4 && 4 - roundCount) || 0}
+        </h3>
+      </div>
+
+      <div className="comment-block">
+        <img src={mindCloud} alt="mind-cloud" className="mind-cloud" />
+        <div className="comment rick-and-morty-font">{comment}</div>
+      </div>
+
+      <div className="emotions-blosk">
+        <img src={emotionsEmage} alt="emotions-pic" className="emotion-pic" />
+        <Button
+          text={arrowsRotate}
+          onClick={() => setIsReactedCharacterChange((prev) => !prev)}
+          className="change-character"
+        />
+      </div>
+
+      <div className="score rick-and-morty-font">
+        <p>wins: {numberOfWins}</p>
+        <p>losses: {numberOfLosses}</p>
+      </div>
+    </div>
+  );
 }
 export default ScoreTable;
--- src/routes/Quiz/Quiz.tsx
+++ src/routes/Quiz/Quiz.tsx
@@ -1,47 +1,50 @@
-import './Quiz.css';
+import "./Quiz.css";
 import QuestionBox from "../../components/QuestionBox/QuestionBox";
 import ScoreTable from "../../components/ScoreTable/ScoreTable";
-import QuizResult from '../../components/QuizResult/QuizResult';
-import { useEffect, useState } from 'react';
+import QuizResult from "../../components/QuizResult/QuizResult";
+import { useEffect, useState } from "react";
 
 function Quiz() {
-    const [round, setRound] = useState<number>(1);
-    const [numberOfWins, setNumberOfWins] = useState<number>(0);
-    const [numberOfLosses, setNumberOfLosses] = useState<number>(0);
+  const [round, setRound] = useState<number>(1);
+  const [numberOfWins, setNumberOfWins] = useState<number>(0);
+  const [numberOfLosses, setNumberOfLosses] = useState<number>(0);
 
-    const [showResult, setShowResult] = useState<boolean>(false);
+  const [showResult, setShowResult] = useState<boolean>(false);
 
-    const getScore = (wins: number, losses: number) => {
-        setNumberOfWins(prev=>prev+wins);
-        setNumberOfLosses(prev=>prev+losses);
-        setRound(prev =>prev+1);   
+  const getScore = (wins: number, losses: number) => {
+    setNumberOfWins((prev) => prev + wins);
+    setNumberOfLosses((prev) => prev + losses);
+    setRound((prev) => prev + 1);
+  };
+
+  useEffect(() => {
+    if (round > 3) {
+      setTimeout(() => setShowResult(true), 1000);
     }
+  }, [round]);
 
-    useEffect(()=> {
-        if (round > 3){
-            setTimeout(()=>setShowResult(true), 1000);
-        }
-    }, [round])
+  const onReset = () => {
+    setRound(1);
+    setNumberOfWins(0);
+    setNumberOfLosses(0);
+    setShowResult(false);
+  };
 
-    const onReset = () => {
-        setRound(1);
-        setNumberOfWins (0);
-        setNumberOfLosses (0);
-        setShowResult (false);
-    }
+  return (
+    <div className="quiz">
+      {showResult && (
+        <QuizResult
+          result={numberOfWins > numberOfLosses ? true : false}
+          onClick={() => onReset()}
+        />
+      )}
 
-    return (
-        <div className="quiz">
-            {showResult && <QuizResult result={(numberOfWins>numberOfLosses)?true:false} onClick={()=>onReset()}/>}
-            
-            <div className="playing-field">
-                <QuestionBox onClick = {(wins, losses) => getScore(wins, losses)} /> 
-            </div>
-             
-            <ScoreTable wins={numberOfWins} losses={numberOfLosses} round={round}/>
-        </div>
+      <div className="playing-field">
+        <QuestionBox onClick={(wins, losses) => getScore(wins, losses)} />
+      </div>
 
-
-    )
+      <ScoreTable wins={numberOfWins} losses={numberOfLosses} round={round} />
+    </div>
+  );
 }
 export default Quiz;
